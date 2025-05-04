6713031e2354f6f0995580b329544d56c55aad68
thucnguyen1234 <hantruong3009@gmail.com>
2025-05-04 18:05:50 +0700

    fix clo_plo mapping và fix clo api

---
+++ FPTStella/FPTStella.Contracts/DTOs/CLOs/UpdateCLODto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FPTStella.Contracts.DTOs.CLOs
+{
+    /// <summary>
+    /// Data Transfer Object for updating Course Learning Outcomes
+    /// </summary>
+    public class UpdateCLODto
+    {
+        /// <summary>
+        /// The name of the CLO
+        /// </summary>
+        [StringLength(50)]
+        public string? CloName { get; set; }
+
+        /// <summary>
+        /// The updated details of the CLO
+        /// </summary>
+        [StringLength(50)]
+        public string? CloDetails { get; set; }
+
+        /// <summary>
+        /// Learning outcome details
+        /// </summary>
+        [StringLength(50)]
+        public string? LoDetails { get; set; }
+    }
+}
---
+++ FPTStella/FPTStella.Contracts/DTOs/PLOs/PLOWithCurriculumDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FPTStella.Contracts.DTOs.PLOs
+{
+    /// <summary>
+    /// Data Transfer Object for Program Learning Outcomes with curriculum information
+    /// </summary>
+    public class PLOWithCurriculumDto
+    {
+        /// <summary>
+        /// The unique identifier of the PLO
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The name of the PLO
+        /// </summary>
+        public string PloName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The name of the curriculum this PLO belongs to
+        /// </summary>
+        public string CurriculumName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The description of the PLO
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The ID of the curriculum this PLO belongs to
+        /// </summary>
+        public Guid CurriculumId { get; set; }
+    }
+}
