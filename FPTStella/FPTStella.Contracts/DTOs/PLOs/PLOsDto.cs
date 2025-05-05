17fd99aacaa45ffe42b722e20f61c20f56c1cb62
thucnguyen1234 <hantruong3009@gmail.com>
2025-05-05 16:06:25 +0700

    thêm fields

--- FPTStella/FPTStella.Contracts/DTOs/PLOs/PLOsDto.cs
+++ FPTStella/FPTStella.Contracts/DTOs/PLOs/PLOsDto.cs
@@ -10,6 +10,7 @@ namespace FPTStella.Contracts.DTOs.PLOs
     {
         public Guid Id { get; set; }
         public Guid CurriculumId { get; set; }
+        public string CurriculumName { get; set; } = string.Empty;
         public string PloName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
